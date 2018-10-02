7fab5c61a92dad1ee6b3f50909202a1822d63e8a
Hwiyoung <huddy1204@gmail.com>
2018-10-02 11:20:53 +0900

    feat: Add a code for twitter spider

--- Chapter15_Databases/Emaildb.py
+++ Chapter15_Databases/Emaildb.py
@@ -8,6 +8,15 @@ cur.execute('DROP TABLE IF EXISTS Counts')
 cur.execute('''
 CREATE TABLE Counts (email TEXT, count INTEGER)''')
 
+'''
+A triple-quoted string is just that: a string. It has the same properties as a regular string object. There are only two differences:
+
+It supports multiple lines. This saves you from having to place \n every time you want a newline.
+It allows you to have single and double quotes without escaping. This could be useful with SQL commands.
+
+https://stackoverflow.com/questions/19479853/why-do-we-need-to-use-3-quotes-while-executing-sql-query-from-python-cursor
+'''
+
 fname = input('Enter file name: ')
 if (len(fname) < 1): fname = 'mbox-short.txt'
 fh = open(fname)
@@ -15,7 +24,7 @@ for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,)) # type casting to tuple (xx,)
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (email, count)
